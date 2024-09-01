d31b6f5244f4bc07169d223e32c0aed588f90805
Mpuya JH <62043582+jamesmpuya@users.noreply.github.com>
2024-09-02 02:12:21 +0300

    Create user.go

---
+++ controller/user.go
@@ -0,0 +1,40 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/jamesmpuya/tapindigital/tapinapi-gin/config"
+	"github.com/jamesmpuya/tapindigital/tapinapi-gin/models"
+)
+
+func GetUser(c *gin.Context) {
+	var user models.User
+	config.DB.Where("name = ?", c.Param("id")).Find(&user)
+	c.JSON(200, &user)
+}
+
+func GetUsers(c *gin.Context) {
+	users := []models.User{}
+	config.DB.Find(&users)
+	c.JSON(200, &users)
+}
+
+func CreateUser(c *gin.Context) {
+	var user models.User
+	c.BindJSON(&user)
+	config.DB.Create(&user)
+	c.JSON(200, &user)
+}
+
+func DeleteUser(c *gin.Context) {
+	var user models.User
+	config.DB.Where("id = ?", c.Param("id")).Delete(&user)
+	c.JSON(200, &user)
+}
+
+func UpdateUser(c *gin.Context) {
+	var user models.User
+	config.DB.Where("id = ?", c.Param("id")).First(&user)
+	c.BindJSON(&user)
+	config.DB.Save(&user)
+	c.JSON(200, &user)
+}
